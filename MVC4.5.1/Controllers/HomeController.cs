bcaf3189ff86a11ee93472065e55f32c9b9f82eb
UttamNCrypted <uttam.nadiyapara@ncrypted.com>
2015-05-22 15:51:44 +0530

    sms gateway integration

--- MVC4.5.1/Controllers/HomeController.cs
+++ MVC4.5.1/Controllers/HomeController.cs
@@ -13,6 +13,65 @@ namespace MVC4._5._1.Controllers
             return View();
         }
 
+        public ActionResult SendSMS()
+        {
+
+            //  ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            string username = "uttam.nadiyapara@ncrypted.com";
+            string password = "wj0yc";
+            string msgsender = "Ncrypted Technologies";
+            string destinationaddr = "+918866658130";
+            string message = "Hello Uttam";
+
+            ViaNettSMS s = new ViaNettSMS(username, password);
+            ViaNettSMS.Result result;
+            try
+            {
+                result = s.sendSMS(msgsender, destinationaddr, message);
+
+                if (result.Success)
+                {
+                    //response.Write("Message successfully sent");
+                    ViewBag.Message = "Message successfully sent.";
+                }
+                else
+                {
+                    //response.Write("Received error:" + result.ErrorCode + " " + result.ErrorMessage);
+                    ViewBag.Message = "Received error:" + result.ErrorCode + " " + result.ErrorMessage;
+                }
+            }
+            catch (System.Net.WebException ex)
+            {
+                //Error occurred while connecting to server.
+                //response.Write(ex.Message);
+                ViewBag.Message = ex.Message;
+            }
+
+            return View();
+        }
+
+        public ActionResult TestSMSDeliveryReport(string requesttype, int refno, int errorcode)
+        {
+            try
+            {
+                if (errorcode == 0)
+                {
+                    ViewBag.IsDelivered = "SMS Delivered Successfully!";
+                }
+                else
+                {
+                    ViewBag.IsDelivered = "SMS Delivered Unsuccessful!";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                ViewBag.IsDelivered = ex.Message;
+            }
+            return View();
+        }
+
         public ActionResult Test()
         {
             try
